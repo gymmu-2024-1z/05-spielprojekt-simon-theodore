488be19d305c1851de107a28cdcc735032825db5
Azra Donat <e255118@edu.sbl.ch>
2025-05-08 16:28:18 +0200

    Alles.

--- src/game/scenes/levels/level-01.js
+++ src/game/scenes/levels/level-01.js
@@ -2,6 +2,8 @@ import Flower from "../../gameObjects/pickups/flower"
 import Mushroom from "../../gameObjects/pickups/mushroom"
 import Base2DScene from "../base-2d-scene"
 
+
+
 /**
  * Spiellogik für das Level01.
  */
